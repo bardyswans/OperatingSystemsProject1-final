5c3ea17f32ab2bb30feb8ff9acd38c445da3158a
bardyswans <swansonbrady0@gmail.com>
2025-03-16 17:26:12 -0500

    Updated processInput
    updated processInput.js so it now can handle the quantum time for RR

--- cpu-scheduler-final/src/components/ProcessInput.js
+++ cpu-scheduler-final/src/components/ProcessInput.js
@@ -2,10 +2,11 @@ import { useState } from "react";
 
 export default function ProcessInput({ onRunAlgorithm }) {
   const [numProcesses, setNumProcesses] = useState(5);
+  const [timeQuantum, setTimeQuantum] = useState(2);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onRunAlgorithm(numProcesses);
+    onRunAlgorithm(numProcesses, timeQuantum);
   };
 
   return (
@@ -17,6 +18,12 @@ export default function ProcessInput({ onRunAlgorithm }) {
           type="number"
           value={numProcesses}
           onChange={(e) => setNumProcesses(parseInt(e.target.value))}
+        />
+        <label>Time Quantum (for RR):</label>
+        <input
+          type="number"
+          value={timeQuantum}
+          onChange={(e) => setTimeQuantum(parseInt(e.target.value))}
         />
         <button type="submit">Run Algorithm</button>
       </form>
